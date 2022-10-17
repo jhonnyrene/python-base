383c817066d58af88a0e56378df6601b30490d83
Jhonny Rene <jhonnyrquisbert@gmail.com>
2022-10-17 10:49:24 -0300

    Atualizacao 17-10

--- hello.py
+++ hello.py
@@ -1,5 +1,37 @@
 #!/usr/bin/env python3
 
-#Este programa imprime Hello World
+"""
+Hello World Mult Linguas.
 
-print("Hello, World!")
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem
+correspondente
+
+How to use:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+
+"""
+__version__ = "0.0.1"
+__author__ = "Jhonny Rene"
+__license__ = "Unlicense"
+
+import os
+
+current_language = "it_IT"
+msg = "Hello,World!"
+
+if current_language == "pt_BR":
+    msg =  "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+
+print(msg)
